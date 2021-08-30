26785108421bae04e9807d134b0b73cd441363d7
Marius Welk <marius.welk@outlook.de>
2021-08-30 22:05:54 +0200

    Added Git Hub Version Check

--- Percue/App.xaml.cs
+++ Percue/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ControlzEx.Theming;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -12,7 +13,7 @@ namespace Percue
     /// <summary>
     /// Interaktionslogik für "App.xaml"
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -20,6 +21,38 @@ namespace Percue
 
             // Set the application theme to Dark.Green
             ThemeManager.Current.ChangeTheme(this, "Dark.Blue");
+            var t = Task.Run(() => CheckGitHubNewerVersion());
+        }
+
+        private async Task CheckGitHubNewerVersion()
+        {
+            //Get all releases from GitHub
+            //Source: https://octokitnet.readthedocs.io/en/latest/getting-started/
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("Percue-Updater"));
+            IReadOnlyList<Release> releases = await client.Repository.Release.GetAll("w3lk", "Percue");
+
+            //Setup the versions
+            Version latestGitHubVersion = new Version(releases[0].TagName);
+            Version localVersion = new Version("1.0.2"); //Replace this with your local version. 
+                                                         //Only tested with numeric values.
+
+            //Compare the Versions
+            //Source: https://stackoverflow.com/questions/7568147/compare-version-numbers-without-using-split-function
+            int versionComparison = localVersion.CompareTo(latestGitHubVersion);
+            if (versionComparison < 0)
+            {
+                //The version on GitHub is more up to date than this local release.
+
+            }
+            else if (versionComparison > 0)
+            {
+                //This local version is greater than the release version on GitHub.
+            }
+            else
+            {
+                //This local Version and the Version on GitHub are equal.
+            }
+
         }
 
     }
